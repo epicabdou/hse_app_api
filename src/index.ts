ceb96234be48aba87deaad187e09370aa23cd0e5
abdl'hamid <113943477+epicabdou@users.noreply.github.com>
2025-09-19 22:39:17 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import {clerkClient, clerkMiddleware, getAuth, requireAuth} from '@clerk/express'
+import {verifyWebhook} from "@clerk/express/webhooks";
 
 const app = express()
 app.use(clerkMiddleware())
@@ -33,6 +34,23 @@ app.get('/protected', requireAuth(), async (req, res) => {
     return res.json({ user })
 })
 
+app.post('/api/webhooks', express.raw({ type: 'application/json' }), async (req, res) => {
+    try {
+        const evt = await verifyWebhook(req)
+
+        // Do something with payload
+        // For this guide, log payload to console
+        const { id } = evt.data
+        const eventType = evt.type
+        console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+        console.log('Webhook payload:', evt.data)
+
+        return res.send('Webhook received')
+    } catch (err) {
+        console.error('Error verifying webhook:', err)
+        return res.status(400).send('Error verifying webhook')
+    }
+})
 
 // Health check
 app.get('/healthz', (req, res) => {
