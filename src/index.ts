6b9d17ea6760f1f6524c87ca49fcc4c376fdc283
abdl'hamid <113943477+epicabdou@users.noreply.github.com>
2025-09-23 14:45:18 +0300

    32

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 app.use(express.json({ limit: "10mb" }));
 app.use(clerkMiddleware())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:8081"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET","POST","OPTIONS"],
